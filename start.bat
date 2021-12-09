2db07d5c3496945dd84898758cd61304b70d2bf0
Joakim Olsson <joakim.olsson@addskills.com>
2021-12-09 14:17:23 +0100

    Moved the default server instance over to HTTPS.

--- start.bat
+++ start.bat
@@ -1,2 +1,2 @@
 cd %~pd0\public
-node "%~pd0\node_modules\http-server\bin\http-server" -p 80
+node "%~pd0\node_modules\http-server\bin\http-server" -p 443 -S -C C:\certs\remotelabs.cornerstone.se-crt.pem -K C:\certs\remotelabs.cornerstone.se-key.pem
