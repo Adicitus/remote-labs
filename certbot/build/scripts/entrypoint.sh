f8f3747a2157b6fa1edc64a72f2a0aa4cc729473
Joakim Olsson <joakim.olsson@cornerstone.se>
2022-01-03 10:35:54 +0100

    Fixed the certbot entrypoint script.
    It should now export certificate + key to the prooxy properly.

--- certbot/build/scripts/entrypoint.sh
+++ certbot/build/scripts/entrypoint.sh
@@ -6,13 +6,20 @@ do
     if test -d /etc/letsencrypt/live; then
         # Check if the certificate needs to be renewed.
         certbot renew & wait $!
-        sleep 12h
-        else
-            # Attempt to get a certificate for the given domains.
-            certbot certonly --webroot -w /var/www/certbot -d $CERTBOT_DOMAINS --non-interactive --agree-tos -m $CERTBOT_EMAIL & wait $!
-            domains=$(echo $CERTBOT_DOMAINS | tr "," "\n")
-            main_domain=(domains)
-            cp "etc/letsencrypt/live/$main_domain/cert.pem" /certificate_pub/cert.pem
-            cp "etc/letsencrypt/live/$main_domain/privkey.pem" /certificate_pub/key.pem
+    else
+        # Attempt to get a certificate for the given domains.
+        certbot certonly --webroot -w /var/www/certbot -d $CERTBOT_DOMAINS --non-interactive --agree-tos -m $CERTBOT_EMAIL & wait $!
     fi
+    
+    domains=$(echo $CERTBOT_DOMAINS | tr "," "\n")
+    main_domain=$(echo $domains | head -n 1)
+    srcpath="/etc/letsencrypt/live/$main_domain/cert.pem"
+    dstpath="/certificate_pub/cert.pem"
+    echo "Exporting: $srcpath --> $dstpath"
+    cp --update $srcpath $dstpath
+    srcpath="/etc/letsencrypt/live/$main_domain/privkey.pem"
+    dstpath="/certificate_pub/key.pem"
+    echo "Exporting: $srcpath --> $dstpath"
+    cp --update $srcpath $dstpath
+    sleep 12h
 done
